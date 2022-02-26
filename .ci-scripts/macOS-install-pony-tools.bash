4ae17444224e1fd0d85b34d277b8bb1e20fce340
Sean T Allen <sean@seantallen.com>
2022-02-25 19:39:38 -0500

    Update to address PonyTest package being renamed (#40)

--- .ci-scripts/macOS-install-pony-tools.bash
+++ .ci-scripts/macOS-install-pony-tools.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+case "${1}" in
+"release")
+  ;;
+"nightly")
+  ;;
+*)
+  echo "invalid ponyc version"
+  echo "Options:"
+  echo "release"
+  echo "nightly"
+  exit 1
+esac
+
 #
 # Libresll is required for ponyup
 #
@@ -14,5 +27,5 @@ curl --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/ponylang/
 
 export PATH="$HOME/.local/share/ponyup/bin/:$PATH"
 
-ponyup update ponyc release
-ponyup update corral release
+ponyup update ponyc "${1}"
+ponyup update corral "${1}"
